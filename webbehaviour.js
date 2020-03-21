a452a16a3515cee4f39cc5a40786172e728b3c3f
koushikkonda9959 <62243500+koushikkonda9959@users.noreply.github.com>
2020-03-21 21:02:52 +0530

    Add files via upload

---
+++ webbehaviour.js
@@ -0,0 +1,8 @@
+function menu_view() {
+  if (document.getElementById("menu_list").style.display == "none") {
+    document.getElementById("menu_list").style.display = "block";
+  }
+  else{
+    document.getElementById("menu_list").style.display = "none";
+  }
+}
