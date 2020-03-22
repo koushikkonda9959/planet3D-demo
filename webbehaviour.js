6614b159347eb291e4c76959a454d9c7ce4b62cf
koushikkonda9959 <62243500+koushikkonda9959@users.noreply.github.com>
2020-03-22 15:51:27 +0530

    Update webbehaviour.js

--- webbehaviour.js
+++ webbehaviour.js
@@ -1,8 +1,18 @@
+images = document.getElementsByClassName('slides');
+count = 0;
+slide_show();
+function slide_show(){
+for(i=0;i<images.length;i++){
+  images[i].style.display = "none";
+}
+images[count%images.length].style.display = "block";
+count++;
+setTimeout(slide_show, 2000)
+}
 function menu_view() {
   if (document.getElementById("menu_list").style.display == "none") {
     document.getElementById("menu_list").style.display = "block";
-  }
-  else{
+  } else {
     document.getElementById("menu_list").style.display = "none";
   }
 }
